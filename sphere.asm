82015cb6664d17f0613686f9b58448bc4a389d01
Tom Lynch <twhlynch@gmail.com>
2025-09-25 13:30:03 +1000

    improve sphere and make it a subroutine

--- sphere.asm
+++ sphere.asm
@@ -19,78 +19,125 @@
 ; MARK: main
 GETP            ; R0, R1, R2 = X, Y, Z
 
-ADD R0, R0, #15
-ADD R2, R2, #15 ; offset xz by 15
+ADD R0, R0, #15 ; offset x by 15
+ADD R2, R2, #15 ; offset z by 15
 
-; SETUP i,j,k ;;;
-LD R3, RADIUS   ; set R3 to -RADIUS
-NOT R3, R3
-ADD R3, R3, #1
+LD  R3, RADIUS  ; input radius
 
-LD R4, RADIUS   ; set R4 to -RADIUS
-NOT R4, R4
-ADD R4, R4, #1
-
-LD R5, RADIUS   ; set R5 to -RADIUS
-NOT R5, R5
-ADD R5, R5, #1  ; local xyz are now -RADIUS
-
-MAIN                   ; main loop
-
-; CUBE ;;;;;;;;;;;;;;;;;
-    LD  R6, RADIUS     ; set R6 to RADIUS
-    NOT R6, R6         ; negate R6
-    ADD R6, R6, #1     ; twos complement
-
-    ADD R6, R3, R6     ; check X
-    BRnp DONTRESETX    ; if R3 is not -RADIUS dont reset
-    JSR RESETX         ; if R3 is -RADIUS reset X
-
-    LD  R6, RADIUS     ; set R6 to RADIUS
-    NOT R6, R6         ; negate R6
-    ADD R6, R6, #1     ; twos complement
-
-    ADD R6, R4, R6     ; check Y
-    BRnp DONTRESETY    ; if R4 is not -RADIUS dont reset
-    JSR RESETY         ; if R4 is -RADIUS reset X
-
-    LD  R6, RADIUS     ; set R6 to RADIUS
-    NOT R6, R6         ; negate R6
-    ADD R6, R6, #1     ; twos complement
-
-    ADD R6, R5, R6     ; check Z
-    BRz EXIT           ; if R5 is -RADIUS break out of loop to exit
-    JSR INCREMENTZ     ; if R5 is not -RADIUS increment Z then Y then X
-
-    DONTRESETY
-    JSR INCREMENTY     ; increment Y then X
-
-    DONTRESETX
-    JSR INCREMENTX     ; increment X
-
-; PLACE BLOCK ;;;;;;;;;;
-    JSR STORELOCATION  ; save local xyz since we need the registries
-
-    JSR GETDISTANCE    ; get distance from 0,0,0 into R6
-    LD R3 DISTANCE     ; load -100 into R3 -(RADIUS^2)
-
-    ADD R3, R6, R3     ; compare distance to -(RADIUS^2)
-    BRp DONTPLACEBLOCK ; if > RADIUS, don't place block
-
-    LD R3, BLOCK_ID    ; load block id
-    SETB               ; set block at x, y, z
-
-    DONTPLACEBLOCK
-
-    JSR LOADLOCATION   ; restore local xyz into R345
-
-    BR MAIN            ; loop
-
-EXIT
+JSR SPHERE      ; create sphere at R0, R1, R2 of radius R3
 
 HALT
 
 ; MARK: subroutines
+
+SPHERE ; creates a sphere at R0, R1, R2 of radius R3
+
+    ST R7 BACKUP_7
+    ST R6 BACKUP_6
+    ST R5 BACKUP_5
+    ST R4 BACKUP_4
+    ST R3 BACKUP_3
+    ST R2 BACKUP_2
+    ST R1 BACKUP_1
+    ST R0 BACKUP_0
+
+    ST R3 R_INTERNAL      ; backup RADIUS internally
+    NOT R3, R3
+    ADD R3, R3, #1
+    ST R3 R_NEGATIVE      ; save -RADIUS into R_NEGATIVE
+    NOT R3, R3
+    ADD R3, R3, #1
+    JSR COMPUTE_R_SQUARED ; compute -R3^2 into R_SQUARED
+
+
+    ; SETUP i,j,k ;;
+    NOT R3, R3     ; set R3 to -RADIUS
+    ADD R3, R3, #1 ; twos compliment
+
+    ADD R4, R3, #0 ; set R4 to -RADIUS
+    ADD R5, R3, #0 ; set R5 to -RADIUS
+
+    ; local xyz are now all -RADIUS
+
+    MAIN                   ; main loop
+
+        ; PLACE BLOCK ;;;;;;
+        JSR STORELOCATION  ; save local xyz since we need the registries
+
+        JSR GETDISTANCE    ; get distance from 0,0,0 into R6
+        LD R3 R_SQUARED    ; load -100 into R3 -(RADIUS^2)
+
+        ADD R3, R6, R3     ; compare distance to -(RADIUS^2)
+        BRp DONTPLACEBLOCK ; if > RADIUS, don't place block
+
+        LD R3, BLOCK_ID    ; load block id
+        SETB               ; set block at x, y, z
+
+        DONTPLACEBLOCK
+
+        JSR LOADLOCATION   ; restore local xyz into R345
+
+        ; CUBE ;;;;;;;;;;;;;
+        LD  R6, R_NEGATIVE ; set R6 to -RADIUS
+
+        ADD R6, R3, R6     ; check X
+        BRnp DONTRESETX    ; if R3 is not RADIUS dont reset                         ; if (x != RADIUS) x++; continue;
+        JSR RESETX         ; if R3 is RADIUS reset X                                ; else x = -RADIUS;
+
+        LD  R6, R_NEGATIVE ; set R6 to -RADIUS
+
+        ADD R6, R4, R6     ; check Y
+        BRnp DONTRESETY    ; if R4 is not RADIUS dont reset                         ; if (y != RADIUS) y++; continue;
+        JSR RESETY         ; if R4 is RADIUS reset X                                ; else y = -RADIUS;
+
+        LD  R6, R_NEGATIVE ; set R6 to -RADIUS
+
+        ADD R6, R5, R6     ; check Z
+        BRz EXIT           ; if R5 is RADIUS break out of loop to exit              ; if (z == RADIUS) break;
+        JSR INCREMENTZ     ; if R5 is not RADIUS increment Z then Y then X          ; else z++;
+        BR MAIN
+
+        DONTRESETY
+        JSR INCREMENTY     ; increment Y then X
+        BR MAIN
+
+        DONTRESETX
+        JSR INCREMENTX     ; increment X
+
+        BR MAIN            ; loop
+
+    EXIT
+
+    LD R7 BACKUP_7
+    LD R6 BACKUP_6
+    LD R5 BACKUP_5
+    LD R4 BACKUP_4
+    LD R3 BACKUP_3
+    LD R2 BACKUP_2
+    LD R1 BACKUP_1
+    LD R0 BACKUP_0
+
+    RET
+
+COMPUTE_R_SQUARED ; compute -RADIUS^2 into R_SQUARED label
+
+    ADD R4, R3, #0       ; copy radius into R4
+    ADD R5, R3, #0       ; copy radius into R5
+
+    ADD R5, R5, #-1      ; i-- since R4 is already radius
+
+    MLUTPLY_LOOP         ; multiply loop
+        ADD R4, R4, R3   ; result += radius
+        ADD R5, R5, #-1  ; i--
+    BRp MLUTPLY_LOOP     ; repeat radius times so R4 is RADIUS*RADIUS
+
+    NOT R4, R4           ; twos complement
+    ADD R4, R4, #1       ; R4 is now -R^2
+
+    ST  R4, R_SQUARED    ; store the value
+
+    RET
+
 RESETX ; reset local x (R3) and take 2*RADIUS from global x (R0)
 
     NOT R3, R3     ; assume if we are resetting, R3 == RADUS
@@ -144,7 +191,7 @@ INCREMENTZ ; add 1 to local and global z
 
 STORELOCATION ; store local x, y, z in STORE + x0, x1, x2
 
-    LD  R6, STORE  ; load STORE memory location
+    LEA R6, STORE  ; load STORE memory location
 
     STR R3, R6, x0 ; store x in STORE
     STR R4, R6, x1 ; store y in STORE + x1
@@ -154,7 +201,7 @@ STORELOCATION ; store local x, y, z in STORE + x0, x1, x2
 
 LOADLOCATION ; load local x, y, z from STORE + x0, x1, x2
 
-    LD  R6, STORE  ; load STORE memory location
+    LEA R6, STORE  ; load STORE memory location
 
     LDR R3, R6, x0 ; load x from STORE
     LDR R4, R6, x1 ; load x from STORE + x1
@@ -188,7 +235,7 @@ GETDISTANCE ; get distance from R3,4,5 to 0,0,0 into R6
 
 ; SQUARE VALUES ;;;;;;;;; -n + 2n + 2(n-1) + ... + 2(1)
     ADD R6, R3, #0      ; use R6 as index
-    NOT R3, R3          ; twos complimet
+    NOT R3, R3          ; twos complement
     ADD R3, R3, #-1     ; R3 is negative
     SQUARE_R3
         ADD R3, R3, R6  ; add i to R3
@@ -197,7 +244,7 @@ GETDISTANCE ; get distance from R3,4,5 to 0,0,0 into R6
     BRp SQUARE_R3       ; loop though to index = 1
 
     ADD R6, R4, #0      ; use R6 as index
-    NOT R4, R4          ; twos complimet
+    NOT R4, R4          ; twos complement
     ADD R4, R4, #-1     ; R4 is negative
     SQUARE_R4
         ADD R4, R4, R6  ; add i to R4
@@ -206,7 +253,7 @@ GETDISTANCE ; get distance from R3,4,5 to 0,0,0 into R6
     BRp SQUARE_R4       ; loop though to index = 1
 
     ADD R6, R5, #0      ; use R6 as index
-    NOT R5, R5          ; twos complimet
+    NOT R5, R5          ; twos complement
     ADD R5, R5, #-1     ; R4 is negative
     SQUARE_R5
         ADD R5, R5, R6  ; add i to R4
@@ -217,17 +264,26 @@ GETDISTANCE ; get distance from R3,4,5 to 0,0,0 into R6
 ; TOTAL ;;;;;;;;;;;;;;;;;
     ADD R6, R3, R4      ; add R3, and R4
     ADD R6, R6, R5      ; add R5. R6 is now total distance
-    LD R3, RADIUS       ; load RADIUS
-    ADD R6, R6, R3      ; reduce by RADIUS to avoid clipping
+    LD R3, R_INTERNAL   ; load RADIUS
 
     RET
 
 ; MARK: labels
-BLOCK_ID .FILL #95    ; GLASS_BLOCK
+BLOCK_ID  .FILL #1  ; input block ID
+RADIUS    .FILL #10 ; input radius
 
-RADIUS   .FILL #10
-DISTANCE .FILL #-100  ; -RADIUS^2 used for comparison
-
-STORE    .FILL 0x3100 ; location for saving and loading xyz
+; internals for SPHERE subroutine
+R_INTERNAL .BLKW 1     ; location to save RADIUS within subroutine
+R_NEGATIVE .BLKW 1     ; location for saving -RADIUS used for cube
+R_SQUARED  .BLKW 1     ; location for saving -RADIUS^2 used for comparison
+STORE      .BLKW 3     ; location for saving and loading xyz
+BACKUP_7   .BLKW 1     ; location for backing up R7
+BACKUP_6   .BLKW 1     ; location for backing up R6
+BACKUP_5   .BLKW 1     ; location for backing up R5
+BACKUP_4   .BLKW 1     ; location for backing up R4
+BACKUP_3   .BLKW 1     ; location for backing up R3
+BACKUP_2   .BLKW 1     ; location for backing up R2
+BACKUP_1   .BLKW 1     ; location for backing up R1
+BACKUP_0   .BLKW 1     ; location for backing up R0
 
 .END
